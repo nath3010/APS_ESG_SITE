f61e8cb69cc20734dc3fbdb178c6a74a4277207f
Nathalia Dias <nathalia.dias3010@gmail.com>
2022-05-17 00:36:05 -0300

    Add files via upload

---
+++ javascript/myjava.js
@@ -0,0 +1,16 @@
+
+function EnviarContato() {
+    var email = document.getElementById("emailid");
+    
+    if (email.value != "") {
+    alert('Obrigado! A sua mensagem foi enviada com sucesso.');
+    }
+}
+
+function EnviarNewsletter() {
+    var email = document.getElementById("emailid");
+    
+    if (email.value != "") {
+    alert('Obrigado! Você assinou a newsletter.');
+    }
+}
